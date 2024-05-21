11529d6d4011987be77dd9e487c6f5e3658cb972
Kirillmlk <kostakov.kiirill@yandex.by>
2024-05-21 16:03:55 +0300

    add middlewaer for basket

--- routes/web.php
+++ routes/web.php
@@ -33,11 +33,16 @@ Route::middleware(['auth', 'is_admin'])->namespace('App\Http\Controllers\Admin')
 
 Route::get('/', [MainController::class, 'index'])->name('index');
 
-Route::get('/basket', [BasketController::class, 'basket'])->name('basket');
-Route::get('/basket/place', [BasketController::class, 'basketPlace'])->name('basket-place');
-Route::post('/basket/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
-Route::post('/basket/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
-Route::post('/basket/place', [BasketController::class, 'basketConfirm'])->name('basket-confirm');
+Route::prefix('basket')->group(function () {
+    Route::post('/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
+
+    Route::middleware(['basket_not_empty'])->group(function () {
+        Route::get('/', [BasketController::class, 'basket'])->name('basket');
+        Route::get('/place', [BasketController::class, 'basketPlace'])->name('basket-place');
+        Route::post('/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
+        Route::post('/place', [BasketController::class, 'basketConfirm'])->name('basket-confirm');
+    });
+});
 
 
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
